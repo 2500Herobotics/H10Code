7ccf054d00179ff848e41764a6433d0f298c0620
2500Herobotics <herobotics2500@gmail.com>
2017-02-02 16:38:17 -0600

    auto update

--- src/org/usfirst/frc/team2500/robot/Autonomous.java
+++ src/org/usfirst/frc/team2500/robot/Autonomous.java
@@ -9,17 +9,9 @@ public class Autonomous extends IterativeRobot {
      * This function is run once each time the robot enters autonomous mode
      */
 	Begin begin = Robot.begin;
-	Talon gearbox1;
-	Talon gearbox2;
-	Talon gearbox3;
-	double speed = 0;
-	double mult = 1; 
 	
     public void autonomousInit() {
     	Begin.autoLoopCounter = 0;
-	    gearbox1 = new Talon(3);
-	    gearbox2 = new Talon(4);
-	    gearbox3 = new Talon(5);
 	    
 	    
     }
@@ -28,15 +20,13 @@ public class Autonomous extends IterativeRobot {
      * This function is called periodically during autonomous
      */
     public void autonomousPeriodic() {
-    	if (Math.abs(speed) >= 1){
-    		speed = 0;
-    		mult *= -1;
+    	if(Begin.autoLoopCounter < 50){
+    		begin.driveTrain1.tankDrive(1, 1);
+    		begin.driveTrain2.tankDrive(1, 1);
     	}
-    	gearbox1.set(speed);
-    	gearbox2.set(speed);
-    	gearbox3.set(speed);
-    	speed += 0.01 * mult;
-    	
-    	
+    	else {
+    		begin.driveTrain1.tankDrive(0, 0);
+    		begin.driveTrain2.tankDrive(0, 0);
+    	}
     }
 }
