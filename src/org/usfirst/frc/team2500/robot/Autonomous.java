45915a29b2a2fbba8e6084b91644f8cbe0b4d108
2500Herobotics <herobotics2500@gmail.com>
2017-02-08 15:36:39 -0600

    auto

--- src/org/usfirst/frc/team2500/robot/Autonomous.java
+++ src/org/usfirst/frc/team2500/robot/Autonomous.java
@@ -5,13 +5,17 @@ import edu.wpi.first.wpilibj.Talon;
 
 
 public class Autonomous extends IterativeRobot {
-    /**
+    double speed = 0;
+	double target = 80;
+	double k = -0.1;
+	/**
      * This function is run once each time the robot enters autonomous mode
      */
-	Begin begin = Robot.begin;
+	Begin begin;
 	
     public void autonomousInit() {
-    	Begin.autoLoopCounter = 0;
+		begin = Robot.begin;
+    	begin.eCodeLeft.reset();
 	    
     }
 
@@ -19,11 +23,17 @@ public class Autonomous extends IterativeRobot {
      * This function is called periodically during autonomous
      */
     public void autonomousPeriodic() {
-//    	if(begin.eCodeLeft.getRaw() < 1000){
-//    		
-//    	}
-//    	else{
-//    	
-//    	}
+    	double error = target - begin.eCodeLeft.getDistance();
+    	System.out.println("Encoder: " + begin.eCodeLeft.getDistance());
+    	speed = error * k;
+    	if(error > 0.25){
+    		begin.driveTrain1.drive(speed, 0);
+    		begin.driveTrain2.drive(speed, 0);
+    	}
+    	else{
+    		begin.driveTrain1.drive(speed, 0);
+    		begin.driveTrain2.drive(speed, 0);
+    	}
+    	
     }
 }
