f56628ce038164e8fa7808ddcdf65c5b21fcd14b
2500Herobotics <herobotics2500@gmail.com>
2017-02-22 15:24:03 -0600

    whu

--- src/org/usfirst/frc/team2500/robot/AutoMid.java
+++ src/org/usfirst/frc/team2500/robot/AutoMid.java
@@ -7,10 +7,16 @@ import edu.wpi.first.wpilibj.command.Command;
 
 public class Automid extends Command {
 	double speed = 0;
-	double target1 = 180;
+	double target1 = 45;
+//	double target1 = 180;
+	double target2 = -10;
+	
+	int count;
 
 	boolean reach1;
 	boolean reach2;
+	boolean reach3;
+	boolean reach4;
 	
 	double k = -0.1;
 	/**
@@ -24,6 +30,10 @@ public class Automid extends Command {
     	begin.eCodeRight.reset();
 //    	drive = new eCodeDrive();
     	reach1 = false;
+    	reach2 = false;
+    	reach3 = false;
+    	reach4 = false;
+    	count = 0;
     }
 
     /**
@@ -31,14 +41,31 @@ public class Automid extends Command {
      */
     public void execute() {
     	
-    	if(!reach1){
-    		if(begin.drive.driveDistance(target1, 1, 0.5)){
-        		reach1 = true;
-    		}
-    	}
-    	else{
-    		begin.drive.arcadeDrive(0, 0);
-    		reach2 = true;
+    	begin.light1.set(begin.sol1.get());
+		begin.light2.set(begin.sol1.get());
+    	begin.sol1.set(true);
+    	if(begin.eCodeLeft.getDistance() < 60){
+    		if(!reach1){
+        		if(begin.drive.driveDistance(target1, 0.5, 0.5)){
+            		reach1 = true;
+            		begin.drive.arcadeDrive(0, 0);
+            		begin.sol3.set(true);
+        		}
+        	}
+        	else if(!reach2) {
+        		if (count >= 20){
+            		if(begin.drive.driveDistance(target2, 1, 0.5)){
+            			reach2 = true;
+            			begin.drive.arcadeDrive(0, 0);
+            		}
+        		}
+        		else count++;
+        	}
+    		
+        	else{
+        		begin.drive.arcadeDrive(0, 0);
+        		reach3 = true;
+        	}
     	}
     	
     	System.out.println("Left Distance: " + begin.eCodeLeft.getDistance());
@@ -47,6 +74,6 @@ public class Automid extends Command {
     
 	@Override
 	protected boolean isFinished() {
-		return reach2;
+		return reach3;
 	}
 }
